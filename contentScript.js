5133190ec0d8a60b0e85fd65b55277a94c846b88
Evan Morikawa <evan@evanmorikawa.com>
2018-07-27 15:18:28 -0400

    upgrade script to support more buttons

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,5 @@
+const CHECK_INTERVAL_SEC = 2;
+
 function doClick(el) {
   console.log("Ensuring click", el)
   const eargs = {bubbles: true, cancelable: true, view: window}
@@ -11,12 +13,19 @@ function findBtnWithText(text) {
   return Array.from(document.querySelectorAll('div[role="button"]')).filter((b) => b.innerText.toLowerCase().trim() == text)[0]
 }
 
-function ensureClicks() {
-  const joinBtn = findBtnWithText('join')
-  const stillHereBtn = findBtnWithText('yes')
-
-  if (joinBtn) doClick(joinBtn)
-  if (stillHereBtn) doClick(joinBtn)
+function reloadAtMidnight() {
+  const now = new Date();
+  if (now.getHours() == 0 && now.getMinutes() == 0 && now.getSeconds() <= CHECK_INTERVAL_SEC + 1) {
+    window.location.reload();
+  }
 }
 
-setInterval(ensureClicks, 2000)
+function checkLoop() {
+  ['join', 'yes', 'try again', 'ok'].forEach((btnText) => {
+    const btn = findBtnWithText(btnText)
+    if (btn) doClick(btn)
+  })
+  reloadAtMidnight()
+}
+
+setInterval(checkLoop, CHECK_INTERVAL_SEC * 1000)
