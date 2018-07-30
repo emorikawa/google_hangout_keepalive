a823658bb9aac4e6a33a8887aa22afc0f5d0fe1d
Evan Morikawa <evan@evanmorikawa.com>
2018-07-30 11:15:19 -0400

    can login to google too

--- contentScript.js
+++ contentScript.js
@@ -1,16 +1,21 @@
 const CHECK_INTERVAL_SEC = 2;
+const EMAIL = ''
+const PASSWORD = ''
 
 function doClick(el) {
+  if (!el) return
   console.log("Ensuring click", el)
   const eargs = {bubbles: true, cancelable: true, view: window}
   const down = new MouseEvent("mousedown", eargs);
   const up = new MouseEvent("mouseup", eargs);
+  const click = new MouseEvent("click", eargs);
   setTimeout(() => el.dispatchEvent(down), 0)
   setTimeout(() => el.dispatchEvent(up), 0)
+  setTimeout(() => el.dispatchEvent(click), 0)
 }
 
-function findBtnWithText(text) {
-  return Array.from(document.querySelectorAll('div[role="button"]')).filter((b) => b.innerText.toLowerCase().trim() == text)[0]
+function findWithText(text, selector='div[role="button"]') {
+  return Array.from(document.querySelectorAll(selector)).filter((b) => b.innerText.toLowerCase().trim() == text)[0]
 }
 
 function reloadAtMidnight() {
@@ -22,10 +27,33 @@ function reloadAtMidnight() {
 
 function checkLoop() {
   ['join', 'yes', 'try again', 'ok'].forEach((btnText) => {
-    const btn = findBtnWithText(btnText)
+    const btn = findWithText(btnText)
     if (btn) doClick(btn)
   })
+  signIntoGoogle()
   reloadAtMidnight()
+}
+
+function signIntoGoogle() {
+  if (findWithText('choose an account', '#headingText')) {
+    const chooseAccount = findWithText(EMAIL, 'p[role="heading"]')
+    doClick(chooseAccount)
+  }
+
+  const password = document.querySelector('input[type="password"]')
+  if (password) {
+    const curUser = document.getElementById('profileIdentifier')
+    if (curUser && curUser.innerText !== EMAIL) {
+      doClick(curUser)
+    } else {
+      password.value = PASSWORD
+      doClick(findWithText('next'))
+    }
+  }
+
+  if (/interstitials/gi.test(window.location.href)) {
+    doClick(findWithText('do this later', 'a'))
+  }
 }
 
 setInterval(checkLoop, CHECK_INTERVAL_SEC * 1000)
